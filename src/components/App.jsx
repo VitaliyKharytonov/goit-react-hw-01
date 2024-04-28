957d2f9fd19f15eeabaa1c9f2d0070b06394e1e4
VitaliyKharytonov <vitalicaxaritonov@gmail.com>
2024-04-28 19:56:01 +0300

    init project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,8 @@
-import Profile from "./Profile";
 
 export default function App() {
   return (
     <div>
       <h1>Products</h1>
-
-      <Profile />
-      <Profile />
-      <Profile />
     </div>
   );
 }
